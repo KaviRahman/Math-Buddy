ddbdf87a682e1993f3a7f71df65159d3597e3281
KaviRahman <kaviprahman@gmail.com>
2024-07-11 12:00:26 -0500

    fixed

--- app/multiply.js
+++ app/multiply.js
@@ -32,7 +32,7 @@ function generateEquation() {
     let allAnswers = [];
     let switchAnswers = [];
 
-    answer = num1 + num2;
+    answer = num1 * num2;
 
     document.getElementById("num1").innerHTML = num1;
     document.getElementById("num2").innerHTML = num2;
